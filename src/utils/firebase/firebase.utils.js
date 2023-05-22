433f007937885bffd2e0c10fec9ef96ac1ba10ba
Ganesh N. Jaiwal <ganesh.jaiwal05@gmail.com>
2023-05-22 20:52:59 +0530

    Added ENV and favicon.ico

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,12 +23,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSHAohHwbP3yuZTKctwWomCn9j4zVRlLc",
-  authDomain: "crwn-clothing-db-react-zmt.firebaseapp.com",
-  projectId: "crwn-clothing-db-react-zmt",
-  storageBucket: "crwn-clothing-db-react-zmt.appspot.com",
-  messagingSenderId: "54926755805",
-  appId: "1:54926755805:web:3d4cd29752b72ca1f69834",
+  apiKey: process.env.REACT_APP_API_KYE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
