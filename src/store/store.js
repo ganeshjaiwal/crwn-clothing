a1678621afa57c1e90c59491e3eec2ccf19aa165
Ganesh N. Jaiwal <ganesh.jaiwal05@gmail.com>
2023-05-27 16:35:53 +0530

    Added redux-persist and condition to use middleware as per env

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 const composedEnhancer = compose(applyMiddleware(...middleWares));
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blackList: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // root reducer
-export const store = createStore(rootReducer, undefined, composedEnhancer);
+export const store = createStore(persistedReducer, undefined, composedEnhancer);
+
+export const persister = persistStore(store);
